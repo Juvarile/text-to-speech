320b4454361d63123f724e1925385b5e6096a9e5
Julian van Riel <julian.vanriel@student.fontys.nl>
2020-06-23 13:47:58 +0200

    tts en highlight werkend

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
-const maxCharacters = 1000;
 const url = 'https://api.voicerss.org/';
 const apiKey = 'a0f50ac64f4747c0b2966b45f8fe4831';
 const whitespaceError = 'Text must contain text characters other than empty spaces';
-const buildUrl = (str) => `${url}?key=${apiKey}&src=${str}&f=48khz_16bit_stereo`;
-const emptyString = (str) => str.split('').every(_char => _char === ' ' || _char === '/n');
-
+const buildUrl = (str) => `${url}?key=${apiKey}&hl=en-us&src=${str}&f=48khz_16bit_stereo`;
 const playBtn = () => document.getElementById('play-btn');
-const textInput = () => document.getElementById("text-input").innerHTML = "Welcome to our chatbot, in times where privacy has been an ever growing concern for a lot of people it may be interesting to know what the general moral balance is. Take COVID-19 as an example, should we, for example, have to download an app that knows our location and medical information to provide a more ‘safe’ environment for others and give away some privacy to achieve that? Some of you may say ‘Yes! Ofcourse!, while others won’t even think about it, just because of the privacy concerns it brings. To get more insight into these topics, what's ‘right’ and what’s ‘wrong’ in some people's eyes, we have developed this chatbot. By interacting with it, you’ll indirectly help in understanding privacy and moral choices based around important topics, like COVID-19, so thank you for taking your time to help!";
 
 const appContainer = () => document.getElementById('app-container');
 const errorContainer = () => document.getElementById('error-messages');
 const clearErrors = () => errorContainer().innerHTML = '';
 
-const container = document.querySelector('#input-container');
-const listenerFn = ($event) => {
-    if($event.target.value || $event.type === 'paste')
-        playBtn().disabled = false;
-    else
-        playBtn.disabled = true;
-};
+const container = () => document.querySelector('#input-container');
+
+
 
 const displayErrorMsg = (val) => {
 const errs = [];
@@ -30,19 +22,23 @@ errs.forEach(_err => {
     });
 };
 playBtn().addEventListener('click', () => {
-      container().value = container().value.trim();
-      new Audio(buildUrl(container().value)).play();
+   
+      container().value = container().innerText.trim();
+      new Audio(buildUrl(container().innerText)).play();
+      const luminator = lumin(container());
+      luminator.start(50000); // 50000ms to highlight//
+      
+     
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const containerHeight = appContainer().clientHeight;
-    const docHeight = window.innerHeight;
-    appContainer().style.marginTop= `${docHeight/2 - containerHeight/2-25}`
-    textInput().addEventListener('keyup', listenerFn);
-    textInput().addEventListener('paste', listenerFn);
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const containerHeight = appContainer().clientHeight;
+//     const docHeight = window.innerHeight;
+//     appContainer().style.marginTop= `${docHeight/2 - containerHeight/2-25}`
+//     textInput().addEventListener('keyup', listenerFn);
+//     textInput().addEventListener('paste', listenerFn);
+// });
 
-const luminator = lumin(container);
-luminator.start(15000); // 5000ms to highlight
+
 
 
